5870073fa38a6aed2284a87c72813eb10819d919
Marie Brossay <mlfbrossay@gwu.edu>
2019-01-22 16:53:39 -0500

    change order?

--- Changing_Code.py
+++ Changing_Code.py
@@ -12,6 +12,40 @@ import datetime
 app = Flask(__name__)
 ask = Ask(app, "/switch")
 
+def receive_data():
+    while (True):
+        #Read one byte at a time
+        if (ser.inWaiting() > 0):
+            character = ser.read()
+            asciiOrd = ord(character)
+            #If it is a start sequence and we have already started,
+            #start over.
+            if (asciiOrd == 60 and start == True):
+                temp = []
+            #If it is a start sequence and we have not started,
+            #start now
+            elif (asciiOrd == 60 and start == False):
+                start = True
+
+            #If it is not a start or a stop, and we have started,
+            #simply append.
+            elif (asciiOrd != 60 and asciiOrd !=62 and start == True):
+                temp.append(character.decode('ascii'))
+            #If it is an end character, and we have started then we are done.
+            elif (asciiOrd == 62 and start == True):
+
+                #If there is something there, and it is a proper float
+                if len(temp) > 0:
+                    try:
+                        converted = float(''.join(temp))
+                        saveReading(converted)
+                        #Acknowledge receipt of data
+                        ser.write('<5>'.encode('utf-8'))
+                    except Exception as e:
+                        print(e)
+                start = False
+                temp = []
+
 def switch_on():	#Turns on switch
     print('switch has turned on')
     ser.write('<1>'.encode('utf-8'))
@@ -62,37 +96,3 @@ start = False
             
 if __name__ == '__main__':
     app.run(debug = True)
-    
-def receive_data():
-    while (True):
-        #Read one byte at a time
-        if (ser.inWaiting() > 0):
-            character = ser.read()
-            asciiOrd = ord(character)
-            #If it is a start sequence and we have already started,
-            #start over.
-            if (asciiOrd == 60 and start == True):
-                temp = []
-            #If it is a start sequence and we have not started,
-            #start now
-            elif (asciiOrd == 60 and start == False):
-                start = True
-
-            #If it is not a start or a stop, and we have started,
-            #simply append.
-            elif (asciiOrd != 60 and asciiOrd !=62 and start == True):
-                temp.append(character.decode('ascii'))
-            #If it is an end character, and we have started then we are done.
-            elif (asciiOrd == 62 and start == True):
-
-                #If there is something there, and it is a proper float
-                if len(temp) > 0:
-                    try:
-                        converted = float(''.join(temp))
-                        saveReading(converted)
-                        #Acknowledge receipt of data
-                        ser.write('<5>'.encode('utf-8'))
-                    except Exception as e:
-                        print(e)
-                start = False
-                temp = []
