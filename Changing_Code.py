0a9e12c0fcfba6bd047c70134b5e5e28000c616b
Marie Brossay <mlfbrossay@gwu.edu>
2019-01-22 16:39:46 -0500

    testing

--- Changing_Code.py
+++ Changing_Code.py
@@ -1,6 +1,7 @@
 import serial
 import numpy as np
 import time
+import threading
 
 ser = serial.Serial('/dev/rfcomm0', 9600)
 
@@ -28,6 +29,7 @@ def homepage():
 @ask.launch		#This occurs when the user says "Alexa, launch (...)"
 def start_skill():
 	welcome_message = 'Hello, would you like to turn your switch on or off?'
+    t = threading.Thread(target = receive_data)
 	return question(welcome_message)	#Alexa will ask the above question
 
 @ask.intent("OnIntent")		#If the user says "On," this will run
@@ -61,36 +63,36 @@ start = False
 if __name__ == '__main__':
     app.run(debug = True)
     
+def receive_data():
+    while (True):
+        #Read one byte at a time
+        if (ser.inWaiting() > 0):
+            character = ser.read()
+            asciiOrd = ord(character)
+            #If it is a start sequence and we have already started,
+            #start over.
+            if (asciiOrd == 60 and start == True):
+                temp = []
+            #If it is a start sequence and we have not started,
+            #start now
+            elif (asciiOrd == 60 and start == False):
+                start = True
 
-while (True):
-    #Read one byte at a time
-    if (ser.inWaiting() > 0):
-        character = ser.read()
-        asciiOrd = ord(character)
-        #If it is a start sequence and we have already started,
-        #start over.
-        if (asciiOrd == 60 and start == True):
-            temp = []
-        #If it is a start sequence and we have not started,
-        #start now
-        elif (asciiOrd == 60 and start == False):
-            start = True
+            #If it is not a start or a stop, and we have started,
+            #simply append.
+            elif (asciiOrd != 60 and asciiOrd !=62 and start == True):
+                temp.append(character.decode('ascii'))
+            #If it is an end character, and we have started then we are done.
+            elif (asciiOrd == 62 and start == True):
 
-        #If it is not a start or a stop, and we have started,
-        #simply append.
-        elif (asciiOrd != 60 and asciiOrd !=62 and start == True):
-            temp.append(character.decode('ascii'))
-        #If it is an end character, and we have started then we are done.
-        elif (asciiOrd == 62 and start == True):
-
-            #If there is something there, and it is a proper float
-            if len(temp) > 0:
-                try:
-                    converted = float(''.join(temp))
-                    saveReading(converted)
-                    #Acknowledge receipt of data
-                    ser.write('<5>'.encode('utf-8'))
-                except Exception as e:
-                    print(e)
-            start = False
-            temp = []
+                #If there is something there, and it is a proper float
+                if len(temp) > 0:
+                    try:
+                        converted = float(''.join(temp))
+                        saveReading(converted)
+                        #Acknowledge receipt of data
+                        ser.write('<5>'.encode('utf-8'))
+                    except Exception as e:
+                        print(e)
+                start = False
+                temp = []
