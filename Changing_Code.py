558bb6bc09262bb23f4f4439aa7b7d77d7c89438
Marie Brossay <mlfbrossay@gwu.edu>
2019-02-03 14:37:12 -0500

    organizing code

--- Changing_Code.py
+++ Changing_Code.py
@@ -1,22 +1,20 @@
 import serial
 import numpy as np
 import time
-from threading import Thread
-
-ser = serial.Serial('/dev/rfcomm0', 9600)
-
 from flask import Flask
 from flask_ask import Ask, statement, question, session
 import datetime
 
+ser = serial.Serial('/dev/rfcomm0', 9600)
+
 app = Flask(__name__)
 ask = Ask(app, "/switch")
 
-t = Thread(target = receive_data)
-t.setDaemon(True)
-t.start()
+print('------ ', datetime.datetime.now(), ' Program started')   #Will print when program initially runs
 
-startbool = False
+print("Waiting for data...")
+temp = []
+start = False
 
 def switch_on():    #Turns on switch
     print('switch has turned on')
@@ -25,8 +23,6 @@ def switch_on():    #Turns on switch
 def switch_off():   #Turns off switch
     print('switch has turned off')
     ser.write('<0>'.encode('utf-8'))
-
-print('------ ', datetime.datetime.now(), ' Program started')   #Will print when program initially runs
 
 @app.route('/')
 def homepage():
@@ -74,45 +70,39 @@ def saveReading(temperature):
     print('Saving new reading: ' + newReading)
     with open('/home/pi/Desktop/temperatureReadings.csv', 'ab') as file:
         file.write(newReading.encode('utf-8'))
-
-print("Waiting for data...")
-temp = []
-startbool = False
-
-            
+        
 if __name__ == '__main__':
     app.run(debug = True)
-    
-def receive_data():
-    while (True):
-        #Read one byte at a time
-        if (ser.inWaiting() > 0):
-            character = ser.read()
-            asciiOrd = ord(character)
-            #If it is a start sequence and we have already started,
-            #start over.
-            if (asciiOrd == 60 and startbool == True):
-                temp = []
-            #If it is a start sequence and we have not started,
-            #start now
-            elif (asciiOrd == 60 and startbool == False):
-                startbool = True
 
-            #If it is not a start or a stop, and we have started,
-            #simply append.
-            elif (asciiOrd != 60 and asciiOrd !=62 and startbool == True):
-                temp.append(character.decode('ascii'))
-            #If it is an end character, and we have started then we are done.
-            elif (asciiOrd == 62 and startbool == True):
+while (True):
+    #Read one byte at a time
+    if (ser.inWaiting() > 0):
+        character = ser.read()
+        asciiOrd = ord(character)
+        #If it is a start sequence and we have already started,
+        #start over.
+        if (asciiOrd == 60 and start == True):
+            temp = []
+        #If it is a start sequence and we have not started,
+        #start now
+        elif (asciiOrd == 60 and start == False):
+            start = True
 
-                #If there is something there, and it is a proper float
-                if len(temp) > 0:
-                    try:
-                        converted = float(''.join(temp))
-                        saveReading(converted)
-                        #Acknowledge receipt of data
-                        ser.write('<5>'.encode('utf-8'))
-                    except Exception as e:
-                        print(e)
-                startbool = False
-                temp = []
+        #If it is not a start or a stop, and we have started,
+        #simply append.
+        elif (asciiOrd != 60 and asciiOrd !=62 and start == True):
+            temp.append(character.decode('ascii'))
+        #If it is an end character, and we have started then we are done.
+        elif (asciiOrd == 62 and start == True):
+
+            #If there is something there, and it is a proper float
+            if len(temp) > 0:
+                try:
+                    converted = float(''.join(temp))
+                    saveReading(converted)
+                    #Acknowledge receipt of data
+                    ser.write('<5>'.encode('utf-8'))
+                except Exception as e:
+                    print(e)
+            start = False
+            temp = []
