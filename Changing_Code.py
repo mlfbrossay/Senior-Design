e81b959982407d2cffcc67813c40251a8463a8b6
Marie Brossay <mlfbrossay@gwu.edu>
2019-02-03 16:06:20 -0500

    f

--- Changing_Code.py
+++ Changing_Code.py
@@ -15,48 +15,6 @@ print('------ ', datetime.datetime.now(), ' Program started')   #Will print when
 
 print("Waiting for data...")
 
-def readFrom():
-	start = False
-	temp = []
-	while (True):
-	    #Read one byte at a time
-	    if (ser.inWaiting() > 0):
-	        character = ser.read()
-	        asciiOrd = ord(character)
-	        #If it is a start sequence and we have already started,
-	        #start over.
-	        if (asciiOrd == 60 and start == True):
-	            temp = []
-	        #If it is a start sequence and we have not started,
-	        #start now
-	        elif (asciiOrd == 60 and start == False):
-	            start = True
-
-	        #If it is not a start or a stop, and we have started,
-	        #simply append.
-	        elif (asciiOrd != 60 and asciiOrd !=62 and start == True):
-	            temp.append(character.decode('ascii'))
-	        #If it is an end character, and we have started then we are done.
-	        elif (asciiOrd == 62 and start == True):
-
-	            #If there is something there, and it is a proper float
-	            if len(temp) > 0:
-	                try:
-	                    converted = float(''.join(temp))
-	                    saveReading(converted)
-	                    #Acknowledge receipt of data
-	                    ser.write('<5>'.encode('utf-8'))
-	                except Exception as e:
-	                    print(e)
-	            start = False
-	            temp = []
-
-if __name__ == '__main__':
-    app.run(debug = True)
-    thread = Thread(target = readFrom)
-    thread.start()
-    print("is it getting here")
-
 def regular():
 	def switch_on():    #Turns on switch
 	    print('switch has turned on')
@@ -108,6 +66,42 @@ def regular():
 	    print('Saving new reading: ' + newReading)
 	    with open('/home/pi/Desktop/temperatureReadings.csv', 'ab') as file:
 	        file.write(newReading.encode('utf-8'))
+
+def readFrom():
+	start = False
+	temp = []
+	while (True):
+	    #Read one byte at a time
+	    if (ser.inWaiting() > 0):
+	        character = ser.read()
+	        asciiOrd = ord(character)
+	        #If it is a start sequence and we have already started,
+	        #start over.
+	        if (asciiOrd == 60 and start == True):
+	            temp = []
+	        #If it is a start sequence and we have not started,
+	        #start now
+	        elif (asciiOrd == 60 and start == False):
+	            start = True
+
+	        #If it is not a start or a stop, and we have started,
+	        #simply append.
+	        elif (asciiOrd != 60 and asciiOrd !=62 and start == True):
+	            temp.append(character.decode('ascii'))
+	        #If it is an end character, and we have started then we are done.
+	        elif (asciiOrd == 62 and start == True):
+
+	            #If there is something there, and it is a proper float
+	            if len(temp) > 0:
+	                try:
+	                    converted = float(''.join(temp))
+	                    saveReading(converted)
+	                    #Acknowledge receipt of data
+	                    ser.write('<5>'.encode('utf-8'))
+	                except Exception as e:
+	                    print(e)
+	            start = False
+	            temp = []	        
 
 if __name__ == '__main__':
     app.run(debug = True)
