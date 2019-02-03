b767202965dd5362953ef845b0d810d2d7131ecd
Marie Brossay <mlfbrossay@gwu.edu>
2019-02-03 15:11:42 -0500

    ok

--- Changing_Code.py
+++ Changing_Code.py
@@ -70,37 +70,36 @@ def saveReading(temperature):
 if __name__ == '__main__':
     app.run(debug = True)
     
+def readFrom():
+	while (True):
+	    #Read one byte at a time
+	    if (ser.inWaiting() > 0):
+	        character = ser.read()
+	        asciiOrd = ord(character)
+	        #If it is a start sequence and we have already started,
+	        #start over.
+	        if (asciiOrd == 60 and start == True):
+	            temp = []
+	        #If it is a start sequence and we have not started,
+	        #start now
+	        elif (asciiOrd == 60 and start == False):
+	            start = True
 
-while (True):
-    print("is it hitting this") 
-    #Read one byte at a time
-    if (ser.inWaiting() > 0):
-        character = ser.read()
-        asciiOrd = ord(character)
-        #If it is a start sequence and we have already started,
-        #start over.
-        if (asciiOrd == 60 and start == True):
-            temp = []
-        #If it is a start sequence and we have not started,
-        #start now
-        elif (asciiOrd == 60 and start == False):
-            start = True
+	        #If it is not a start or a stop, and we have started,
+	        #simply append.
+	        elif (asciiOrd != 60 and asciiOrd !=62 and start == True):
+	            temp.append(character.decode('ascii'))
+	        #If it is an end character, and we have started then we are done.
+	        elif (asciiOrd == 62 and start == True):
 
-        #If it is not a start or a stop, and we have started,
-        #simply append.
-        elif (asciiOrd != 60 and asciiOrd !=62 and start == True):
-            temp.append(character.decode('ascii'))
-        #If it is an end character, and we have started then we are done.
-        elif (asciiOrd == 62 and start == True):
-
-            #If there is something there, and it is a proper float
-            if len(temp) > 0:
-                try:
-                    converted = float(''.join(temp))
-                    saveReading(converted)
-                    #Acknowledge receipt of data
-                    ser.write('<5>'.encode('utf-8'))
-                except Exception as e:
-                    print(e)
-            start = False
-            temp = []
+	            #If there is something there, and it is a proper float
+	            if len(temp) > 0:
+	                try:
+	                    converted = float(''.join(temp))
+	                    saveReading(converted)
+	                    #Acknowledge receipt of data
+	                    ser.write('<5>'.encode('utf-8'))
+	                except Exception as e:
+	                    print(e)
+	            start = False
+	            temp = []
