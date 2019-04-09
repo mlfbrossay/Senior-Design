fd06e68980948c31142e7adb4c616864d6630483
Marie Brossay <mlfbrossay@gwu.edu>
2019-04-09 02:32:27 -0400

    what

--- April_8.py
+++ April_8.py
@@ -222,8 +222,8 @@ def readFrom(q):
 
 
         cur.execute("INSERT INTO plug1 (collection_time,power_data) VALUES (%s,%s)", (1, switch1))
-        cur.execute("INSERT INTO plug2 (collection_time,power_data) VALUES (NOW(),switch2)")
-        cur.execute("INSERT INTO plug3 (collection_time,power_data) VALUES (NOW(),switch3)")
+        cur.execute("INSERT INTO plug2 (collection_time,power_data) VALUES (%s,%s)", (1, switch2))
+        cur.execute("INSERT INTO plug3 (collection_time,power_data) VALUES (%s,%s)", (1, switch3))
 
 
 if __name__ == '__main__':
