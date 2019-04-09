c18c07283c74e01197e65e18d1e3774b5e56842e
Matt Taylor <mtaylor17@gwu.edu>
2019-04-09 16:38:34 -0400

    updating switch code

--- Switch2Code/Switch2Code.ino
+++ Switch2Code/Switch2Code.ino
@@ -10,12 +10,24 @@ const int senderPin = 7; //Tells us we are sending over Serial
 const int receiverPin = 8; //Tells us we are receiving confirmation over Serial
 const int sendSwitch = 2;
 
+int count = 0;
+int flag2 = 0;
+int flag3 = 0;
+int current_flag_2 = 0;
+int current_flag_3 = 0;
+char data[5] = {};
+
 const float FACTOR = 30; //CT Calibration factors
 const float multiplier = 0.0625F;
 
 float degreesC;
-unsigned long timer = 0;
-int sendStatus = 0;
+unsigned long timer2 = 0;
+unsigned long timer3 = 0;
+
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
 void setup() {
   //Pin Modes
@@ -24,6 +36,7 @@ void setup() {
   pinMode(sendSwitch, INPUT_PULLUP);
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
+  pinMode(6, INPUT_PULLUP);
 
   ads.setGain(GAIN_TWO); // +/- 2.048V 1 bit = 0.0624mV
   ads.begin();
@@ -41,67 +54,127 @@ void setup() {
   digitalWrite(10, LOW);
 }
 
-void printMeasure(String prefix, float value, String postfix)
-{
-  Serial.print(prefix);
-  Serial.print(value, 8); // 3 decimals??
-  Serial.print(postfix);
-}
-
 void loop() {
-  
-  //I want to send the current, I want the Raspberry PI to grab it, process it
-  //and send back a message. I don't want to continue spamming the raspberry pi, so
-  //I will only send a signal every 1 seconds.
-  
-  //Non-blocking every 10 seconds.
-  if ((timer == 0 || millis() >= timer) && sendStatus == 1){
-    
-    //Grab the current power usage in watts
-    float power = getPower();
-    printMeasure("Power: ", power, "W \n");  
-    //Send the current power usage. Didn't use readline to avoid blocking problem
-    String sendPower = "<" + String(power, 3) + ">";
-    
-    //Convert to byte array
-    char charArray[sendPower.length() + 1];
-    sendPower.toCharArray(charArray, sendPower.length()+1);
-    
-    btSerial.write(charArray);
-    
+  if ((timer2 == 0 || millis() >= timer2) && current_flag_2 == 1){
+    float power2 = getPower2();
+    String sendPower2 = "<" + String(power2, 3) + ">";
+    char charArray2[sendPower2.length() + 1]; //Convert to byte array
+    sendPower2.toCharArray(charArray2, sendPower2.length()+1);
+    btSerial.write(charArray2);
     //Reset the timer for another 1 seconds.
-    timer = millis() + 1000;
+    timer2 = millis() + 1000;
       
   }
-  
-  checkButton();
- 
-
-  //Received message back from Raspberry.
-  //RPI sends back messages in the form of <5>.
-  //We only check for the numerical.
-  while (btSerial.available()) {
-    //Numbers are between 48 and 57
-    char rpiMessage = btSerial.read();
-    if(rpiMessage == '1'){
-      digitalWrite(9, HIGH);
-    }
-    if(rpiMessage == '0'){
-      digitalWrite(9, LOW);
-    }
-    Serial.println(rpiMessage); //Shows message from RPi
-    
+  if ((timer3 == 0 || millis() >= timer3) && current_flag_3 == 1){
+    float power3 = getPower3();
+    String sendPower3 = "<" + String(power3, 3) + ">";
+    char charArray3[sendPower3.length() + 1]; //Convert to byte array
+    sendPower3.toCharArray(charArray3, sendPower3.length()+1);
+    btSerial.write(charArray3);
+    //Reset the timer for another 1 seconds.
+    timer3 = millis() + 1000;
+      
   }
-    
+//Debouncing
+  int reading = digitalRead(6);
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == LOW) {
+        if(flag2){
+          digitalWrite(9, LOW);
+          flag2 = 0;
+        }
+        else{
+          digitalWrite(9, HIGH);
+          flag2 = 1;
+        }
+      }
+    }
+  }
+  int reading = digitalRead(6);
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == LOW) {
+        if(flag2){
+          digitalWrite(9, LOW);
+          flag2 = 0;
+        }
+        else{
+          digitalWrite(9, HIGH);
+          flag2 = 1;
+        }
+      }
+    }
+  }
+  lastButtonState = reading;
+//Bluetooth receive
+  while (btSerial.available()) {
+    char rpiMessage = btSerial.read();
+    if(rpiMessage == '<'){
+      count = 0;
+      for (int x = 0; x < sizeof(data) / sizeof(data[0]); x++)
+      {
+        data[x] = 0;
+      }
+      data[count] = rpiMessage;
+    }
+    else if(rpiMessage == '>'){
+      data[count] = rpiMessage;
+      if(data[1] == '2'){
+        if(data[3] == '1'){
+          digitalWrite(9, HIGH);
+          flag2 = 1;
+        }
+        else if(data[3] == '0'){
+          digitalWrite(9, LOW);
+          flag2 = 0;
+        }
+        else if(data[3] == '2'){
+          current_flag_2 = 1;
+        }
+      }
+      if(data[1] == '3'){
+        if(data[3] == '1'){
+          digitalWrite(10, HIGH);
+          flag3 = 1;
+        }
+        else if(data[3] == '0'){
+          digitalWrite(10, LOW);
+          flag3 = 0;
+        }
+        else if(data[3] == '2'){
+          current_flag_3 = 1;
+        }
+      }
+        
+    }
+    else{
+      data[count] = rpiMessage;
+    }
+    count ++;
+  } 
 }
 
-//Checks to see if the user wants to keep sending messages.
-void checkButton() {
-  sendStatus = 1;
+float getPower2(){
+  return 60;
 }
 
-float getPower() {
-  /*float voltage;
+float getPower3() {
+  float voltage;
   float current;
   float sum = 0;
   long Time = millis();
@@ -120,7 +193,5 @@ float getPower() {
   float power = 120 * currentRMS;
   
   return(power);
-  */
-  return 80;
 }
 
